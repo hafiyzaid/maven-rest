e7a490f119fea9ab77fb2c21d6e83f90ad2079bd
hafiy <hafiy@merimen.com>
2024-04-02 16:06:28 +0800

    first commit

---
+++ maven/src/main/java/com/maven/GreetingComponent.java
@@ -0,0 +1,11 @@
+package com.maven;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingComponent {
+ 
+    public String getMessage() {
+        return "Hello from GreetingComponent";
+    }
+}
